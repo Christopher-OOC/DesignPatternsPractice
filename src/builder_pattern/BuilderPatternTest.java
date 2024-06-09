a3b87c69cbd2b0bb9d3c5a13c4b309d1c716658c
Christopher-OOC <olojedechristopher24@gmail.com>
2024-06-09 21:26:35 +0100

    commit

--- src/builder_pattern/BuilderPatternTest.java
+++ src/builder_pattern/BuilderPatternTest.java
@@ -2,8 +2,43 @@ package builder_pattern;
 
 public class BuilderPatternTest {
 	
-	public static void main(String[] args) {
+	private volatile Student student;
+	
+	public BuilderPatternTest() {
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				student = Student.Builder.newInstance()
+							.setId(1)
+							.setName("Christopher")
+							.setAddress("Lagos")
+							.build();
+			}
+		});
 		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				student = Student.Builder.newInstance()
+							.setId(2)
+							.setName("Richard")
+							.setAddress("Abeokuta")
+							.build();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+	}
+	
+	public Student getStudent() {
+		return student;
+	}
+	
+	public static void main(String[] args) {
+		BuilderPatternTest test = new BuilderPatternTest();
+		
+		System.out.println(test.getStudent());
 	}
 
 }
