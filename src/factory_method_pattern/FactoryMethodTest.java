1ceb4f6e2f419f40db304b5bb75380ed5f0b244d
Christopher-OOC <olojedechristopher24@gmail.com>
2024-07-06 00:55:30 +0100

    commit

--- src/factory_method_pattern/FactoryMethodTest.java
+++ src/factory_method_pattern/FactoryMethodTest.java
@@ -3,6 +3,16 @@ package factory_method_pattern;
 public class FactoryMethodTest {
 	
 	public static void main(String[] args) {
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
+		System.out.println("helloo");
 	}
 
 }
